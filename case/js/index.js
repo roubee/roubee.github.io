5fafb183af6bb5874c81a8b4a1f6a2a144a8bce2
Roubee <roubee@Roubeede-MacBook-Pro.local>
2016-11-08 00:45:04 +0800

    v1

--- case/js/index.js
+++ case/js/index.js
@@ -1,5 +1,5 @@
 $(function(){
-    $(".dropdown").hover(function() {
+    $('.dropdown').hover(function() {
         $(this).addClass('open');
     },
     function() {
@@ -9,9 +9,26 @@ $(function(){
     do_slide();
 
     $(".service-block").hover(function() {
-    	$(this).find('.service-mask').css('display', 'none');
+		$(this).find('.service-mask').css('display', 'none');
+		var arrow = $(this).find('.service-arrow');
+		var width = parseInt(arrow.css('width').replace('px',''));
+		width = width*-1;
+		$(this).find('.service-arrow').css('background-position', width+'px 0');
+		var icon = $(this).find('.service-icon');
+		width = icon.css('width');
+		var position = icon.css('background-position').split(" ");
+		var x = parseInt(position[0].replace('px',''));
+		x = x-width;
+		icon.css('background-position', x+'px 0');
     }, function() {
-    	$(this).find('.service-mask').css('display', 'block');
+		$(this).find('.service-mask').css('display', 'block');
+		$(this).find('.service-arrow').css('background-position', '0 0');
+		var icon = $(this).find('.service-icon');
+		var width = icon.css('width');
+		var position = icon.css('background-position').split(" ");
+		var x = parseInt(position[0].replace('px',''));
+		x = x+width;
+		icon.css('background-position', x+'px 0');
     });
 });
 
@@ -31,16 +48,16 @@ function currentDiv(n) {
 
 function showDivs(n) {
 	var i;
-	var x = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("demo");
+	var x = document.getElementsByClassName('mySlides');
+	var dots = document.getElementsByClassName('demo');
 	if (n > x.length) {slideIndex = 1}
 	if (n < 1) {slideIndex = x.length}
 	for (i = 0; i < x.length; i++) {
-  	x[i].style.display = "none";
+  	x[i].style.display = 'none';
 	}
 	for (i = 0; i < dots.length; i++) {
-  	dots[i].className = dots[i].className.replace(" w3-white", "");
+  	dots[i].className = dots[i].className.replace(' w3-white', '');
 	}
-	x[slideIndex-1].style.display = "block";
-	dots[slideIndex-1].className += " w3-white";
+	x[slideIndex-1].style.display = 'block';
+	dots[slideIndex-1].className += ' w3-white';
 }
