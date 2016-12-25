47fad2e67b18e9c53c1c6f0494e57b254f85ecab
Roubee <you@example.com>
2016-12-25 21:27:12 +0800

    v1

--- case2/jss/index.js
+++ case2/jss/index.js
@@ -3,6 +3,30 @@ $(function(){
 	$('#sign-step3').hide();
 	$('#sign-step2').removeClass('hide');
 	$('#sign-step3').removeClass('hide');
+
+	$.widget('custom.iconselectmenu', $.ui.selectmenu, {
+      _renderItem: function( ul, item ) {
+        var li = $('<li>'),
+          wrapper = $('<div>', { text: item.label } );
+ 
+        if (item.disabled) {
+          li.addClass('ui-state-disabled');
+        }
+ 
+        $('<span>', {
+          style: item.element.attr('data-style'),
+          'class': 'ui-icon ' + item.element.attr('data-class')
+        })
+          .appendTo(wrapper);
+ 
+        return li.append(wrapper).appendTo(ul);
+      }
+    });
+
+	$('#country-code')
+      .iconselectmenu()
+      .iconselectmenu('menuWidget')
+      .addClass('ui-menu-icons avatar');
 });
 
 function signupStepShow(nowID,direction){
