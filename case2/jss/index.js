6ccc564e2b7cb8b32911af4603b80e64f657102a
Roubee <you@example.com>
2016-12-25 17:20:33 +0800

    v1

--- case2/jss/index.js
+++ case2/jss/index.js
@@ -5,7 +5,23 @@ $(function(){
 	$('#sign-step3').removeClass('hide');
 });
 
-function signupStepShow(nowID,nextID){
-	$('#sign-step'+nowID).hide();
-	$('#sign-step'+nextID).fadeIn('slow');
+function signupStepShow(nowID,direction){
+	if (direction == 'next'){
+		$('#sign-step'+nowID).hide();
+		$('#sign-step'+(nowID+1)).fadeIn('slow');
+
+		$('#sign-step-li'+nowID).removeClass('active');
+		$('#sign-step-li'+nowID).addClass('done');
+		$('#sign-step-li'+(nowID+1)).removeClass('undone');
+		$('#sign-step-li'+(nowID+1)).addClass('active');
+	}else if (direction == 'prev'){
+		$('#sign-step'+nowID).hide();
+		$('#sign-step'+(nowID-1)).fadeIn('slow');
+
+		$('#sign-step-li'+nowID).removeClass('active');
+		$('#sign-step-li'+nowID).addClass('undone');
+		$('#sign-step-li'+(nowID-1)).removeClass('done');
+		$('#sign-step-li'+(nowID-1)).addClass('active');
+	}
+	
 }
