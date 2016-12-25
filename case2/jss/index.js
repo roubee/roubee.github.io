ebfc574f856b4259ee45ac4b038de5243a6f2a2a
Roubee <you@example.com>
2016-12-25 22:58:44 +0800

    v1

--- case2/jss/index.js
+++ case2/jss/index.js
@@ -1,8 +1,15 @@
 $(function(){
 	$('#sign-step2').hide();
 	$('#sign-step3').hide();
+	$('#sign-step4').hide();
 	$('#sign-step2').removeClass('hide');
 	$('#sign-step3').removeClass('hide');
+	$('#sign-step4').removeClass('hide');
+
+	$('#repw2').hide();
+	$('#repw3').hide();
+	$('#repw2').removeClass('hide');
+	$('#repw3').removeClass('hide');
 
 	$.widget('custom.iconselectmenu', $.ui.selectmenu, {
       _renderItem: function( ul, item ) {
@@ -30,22 +37,35 @@ $(function(){
 });
 
 function signupStepShow(nowID,direction){
+	$('#sign-step'+nowID).hide();
+	$('#sign-step-li'+nowID).removeClass('active');
 	if (direction == 'next'){
-		$('#sign-step'+nowID).hide();
 		$('#sign-step'+(nowID+1)).fadeIn('slow');
-
-		$('#sign-step-li'+nowID).removeClass('active');
 		$('#sign-step-li'+nowID).addClass('done');
 		$('#sign-step-li'+(nowID+1)).removeClass('undone');
 		$('#sign-step-li'+(nowID+1)).addClass('active');
-	}else if (direction == 'prev'){
-		$('#sign-step'+nowID).hide();
-		$('#sign-step'+(nowID-1)).fadeIn('slow');
 
-		$('#sign-step-li'+nowID).removeClass('active');
+		if(nowID==3){
+			$('.sign-progressbar-block').css('display','none');
+		}
+	}else if (direction == 'prev'){
+		$('#sign-step'+(nowID-1)).fadeIn('slow');
 		$('#sign-step-li'+nowID).addClass('undone');
 		$('#sign-step-li'+(nowID-1)).removeClass('done');
 		$('#sign-step-li'+(nowID-1)).addClass('active');
+	}
+	
+}
+
+function resetpwStepShow(nowID,direction){
+	$('#repw'+nowID).hide();
+	$('#repw-title'+nowID).addClass('hide');
+	if (direction == 'next'){
+		$('#repw-title'+nowID).removeClass('hide');
+		$('#repw'+(nowID+1)).fadeIn('slow');
+	}else if (direction == 'prev'){
+		$('#repw-title'+nowID).removeClass('hide');
+		$('#repw'+(nowID-1)).fadeIn('slow');
 	}
 	
 }
