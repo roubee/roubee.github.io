f3449976d7fe892b80d8f4bc2b6d341802118fe0
Roubee <you@example.com>
2017-01-02 12:19:09 +0800

    v1

--- case/jss/index.js
+++ case/jss/index.js
@@ -21,8 +21,6 @@ $(function(){
         $(this).removeClass('open');
     });
 
-    do_slide();
-
     $(".service-block").hover(function() {
 		$(this).find('.service-mask').css('display', 'none');
 		var arrow = $(this).find('.service-arrow');
@@ -161,39 +159,4 @@ function openMenu(){
 			$('.nav-right-side').css('display', 'none');
 			$('.level2-block').css('display','none');
 		}
-}
-
-var slideIndex = 1;
-var slideIntervalTime = 5000;
-var interval;
-showDivs(slideIndex);
-
-function do_slide(){
-	interval = setInterval(function(){
-		showDivs(slideIndex += 1);
-	}, slideIntervalTime);
-}
-
-function currentDiv(n) {
-	clearInterval(interval);
-	interval = setInterval(function(){
-		showDivs(slideIndex += 1);
-	}, slideIntervalTime);
-	showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-	var i;
-	var x = document.getElementsByClassName('mySlides');
-	var dots = document.getElementsByClassName('demo');
-	if (n > x.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = x.length}
-	for (i = 0; i < x.length; i++) {
-  	x[i].style.display = 'none';
-	}
-	for (i = 0; i < dots.length; i++) {
-  	dots[i].className = dots[i].className.replace(' w3-white', '');
-	}
-	x[slideIndex-1].style.display = 'block';
-	dots[slideIndex-1].className += ' w3-white';
 }
--- case/jss/w3.js
+++ case/jss/w3.js
@@ -1,26 +1,34 @@
 var slideIndex = 1;
+var slideIntervalTime = 5000;
+var interval;
 showDivs(slideIndex);
 
-function plusDivs(n) {
-	showDivs(slideIndex += n);
+function do_slide(){
+	interval = setInterval(function(){
+		showDivs(slideIndex += 1);
+	}, slideIntervalTime);
 }
 
 function currentDiv(n) {
+	clearInterval(interval);
+	interval = setInterval(function(){
+		showDivs(slideIndex += 1);
+	}, slideIntervalTime);
 	showDivs(slideIndex = n);
 }
 
 function showDivs(n) {
 	var i;
-	var x = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("demo");
+	var x = document.getElementsByClassName('mySlides');
+	var dots = document.getElementsByClassName('demo');
 	if (n > x.length) {slideIndex = 1}
 	if (n < 1) {slideIndex = x.length}
 	for (i = 0; i < x.length; i++) {
-  	x[i].style.display = "none";
+  	x[i].style.display = 'none';
 	}
 	for (i = 0; i < dots.length; i++) {
-  	dots[i].className = dots[i].className.replace(" w3-white", "");
+  	dots[i].className = dots[i].className.replace(' w3-white', '');
 	}
-	x[slideIndex-1].style.display = "block";
-	dots[slideIndex-1].className += " w3-white";
+	x[slideIndex-1].style.display = 'block';
+	dots[slideIndex-1].className += ' w3-white';
 }
