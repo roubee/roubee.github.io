c1b912e83b27ff7759c403c60d24ded85e7ee221
Roubee <you@example.com>
2017-01-08 12:56:47 +0800

    v1

--- case/jss/w3.js
+++ case/jss/w3.js
@@ -19,16 +19,19 @@ function currentDiv(n) {
 
 function showDivs(n) {
 	var i;
-	var x = document.getElementsByClassName('mySlides');
-	var dots = document.getElementsByClassName('demo');
-	if (n > x.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = x.length}
-	for (i = 0; i < x.length; i++) {
-  	x[i].style.display = 'none';
+	var w3Content = document.getElementsByClassName('w3-content');
+	for (var j = 0; j < w3Content.length; j++){
+		var x = w3Content[j].getElementsByClassName('mySlides');
+		var dots = w3Content[j].getElementsByClassName('demo');
+		if (n > x.length) {slideIndex = 1}
+		if (n < 1) {slideIndex = x.length}
+		for (i = 0; i < x.length; i++) {
+	  	x[i].style.display = 'none';
+		}
+		for (i = 0; i < dots.length; i++) {
+	  	dots[i].className = dots[i].className.replace(' w3-white', '');
+		}
+		x[slideIndex-1].style.display = 'block';
+		dots[slideIndex-1].className += ' w3-white';
 	}
-	for (i = 0; i < dots.length; i++) {
-  	dots[i].className = dots[i].className.replace(' w3-white', '');
-	}
-	x[slideIndex-1].style.display = 'block';
-	dots[slideIndex-1].className += ' w3-white';
 }
