4d46293a48f1db47d9ff4db3a7b0d0d76a46c422
Roubee <you@example.com>
2017-01-15 13:37:40 +0800

    v1

--- case/jss/index.js
+++ case/jss/index.js
@@ -16,6 +16,25 @@ $(window).resize(function () {
 	}
 });
 $(function(){
+	$.widget('custom.iconselectmenu', $.ui.selectmenu, {
+      _renderItem: function( ul, item ) {
+        var li = $('<li>'),
+          wrapper = $('<div>', { text: item.label } );
+ 
+        if (item.disabled) {
+          li.addClass('ui-state-disabled');
+        }
+ 
+        $('<span>', {
+          style: item.element.attr('data-style'),
+          'class': 'ui-icon ' + item.element.attr('data-class')
+        })
+          .appendTo(wrapper);
+ 
+        return li.append(wrapper).appendTo(ul);
+      }
+    });
+	
 	window.scrollTo(0,0);
     $('.dropdown').hover(function() {
         $(this).addClass('open');
