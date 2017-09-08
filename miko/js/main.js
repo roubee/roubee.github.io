e82ca48b6329031f585124d76995c79abf75afd8
Roubee <roubee415@gmail.com>
2017-09-08 16:08:18 +0800

    miko修改

--- miko/js/main.js
+++ miko/js/main.js
@@ -4,35 +4,33 @@ var windowWidth = 'innerWidth' in window
 var device = 'mobile';
 var pcSize = 769;
 
-$(function(){
-	if(windowWidth >= pcSize){
-		device = 'pc';
-	}
-});
-
-$(window).resize(function(){
-	windowWidth = 'innerWidth' in window
-        ? window.innerWidth
-        : document.documentElement.clientWidth;
-	if(windowWidth >= pcSize){
-		device = 'pc';
-	}else{
-		device = 'mobile';
-	}
-
-	if(device == 'pc'){
-		$('#nav__bottom-wrap').show();
-	}else{
-		$('#nav__bottom-wrap').hide();
-	}
-});
-
-// $('#nav__btn-hamburger').click(function(){
-// 	if(device == 'mobile'){
-// 		$('#nav__bottom-wrap').toggle();
+// $(function(){
+// 	if(windowWidth >= pcSize){
+// 		device = 'pc';
 // 	}
-// 	return false;
 // });
+
+// $(window).resize(function(){
+// 	windowWidth = 'innerWidth' in window
+//         ? window.innerWidth
+//         : document.documentElement.clientWidth;
+// 	if(windowWidth >= pcSize){
+// 		device = 'pc';
+// 	}else{
+// 		device = 'mobile';
+// 	}
+
+// 	if(device == 'pc'){
+// 		$('#nav__bottom-wrap').show();
+// 	}else{
+// 		$('#nav__bottom-wrap').hide();
+// 	}
+// });
+
+$('#nav__btn-hamburger').click(function(){
+	$('#nav__bottom-wrap').toggle();
+	return false;
+});
 
 $('#footer__btn-totop').click(function(){
 	$('html,body').animate({scrollTop: 0});
