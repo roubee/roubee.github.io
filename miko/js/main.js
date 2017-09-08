dd6abd41320239abe31107d20fd3728ff8775742
Roubee <roubee415@gmail.com>
2017-09-08 16:14:03 +0800

    miko修改

--- miko/js/main.js
+++ miko/js/main.js
@@ -1,32 +1,3 @@
-var windowWidth = 'innerWidth' in window
-    ? window.innerWidth
-    : document.documentElement.clientWidth;
-var device = 'mobile';
-var pcSize = 769;
-
-// $(function(){
-// 	if(windowWidth >= pcSize){
-// 		device = 'pc';
-// 	}
-// });
-
-// $(window).resize(function(){
-// 	windowWidth = 'innerWidth' in window
-//         ? window.innerWidth
-//         : document.documentElement.clientWidth;
-// 	if(windowWidth >= pcSize){
-// 		device = 'pc';
-// 	}else{
-// 		device = 'mobile';
-// 	}
-
-// 	if(device == 'pc'){
-// 		$('#nav__bottom-wrap').show();
-// 	}else{
-// 		$('#nav__bottom-wrap').hide();
-// 	}
-// });
-
 $('#nav__btn-hamburger').click(function(){
 	$('#nav__bottom-wrap').toggle();
 	return false;
