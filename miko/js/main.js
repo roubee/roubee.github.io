4f0813ef84f99527ac2d0a06f023bc615006ae33
Roubee <roubee415@gmail.com>
2018-06-21 22:18:04 +0800

    刪除

--- miko/js/main.js
+++
@@ -1,9 +0,0 @@
-$('#nav__btn-hamburger').click(function(){
-	$('#nav__bottom-wrap').toggle();
-	return false;
-});
-
-$('#footer__btn-totop').click(function(){
-	$('html,body').animate({scrollTop: 0});
-	return false;
-});
